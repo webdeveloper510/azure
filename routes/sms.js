1530de0e85a0eb463a82cd433b79708a696059e0
Developerweb <developerweb@gmail.com>
2021-03-19 01:34:37 +0530

    changes

--- routes/sms.js
+++ routes/sms.js
@@ -9,42 +9,45 @@ const formidable = require('formidable')
 
 
 /* GET users listing. */
-router.post('/sendSms', async function(req, res, next) {
-//   new formidable.IncomingForm().parse(req, (err, fields, files) => {
-//     if (err) {
-//       console.error('Error', err)
-//       throw err
-//     }
-//   //  console.log('Fields', fields)
-//    // console.log('Files', files)
-//   //  var fr=new FileReader(); 
-
-//  //   console.log(fr.readAsText(files[0])); 
-//     // for (const file of Object.entries(files)) {
-//     //   console.log(file)
-//     // }
-//   })
-  //console.log(req.files)
-  //return false
- // const smsClient = new SmsClient(req.body.connectionString);
-  const smsClient = new SmsClient("endpoint=https://lapp.communication.azure.com/;accesskey=tI/HIx5ROdnKciLFE1HaMZ3DLKpg7Mbq38k3rH7wNMI7xkKIc9B8Qxk5pTpr6dCpf19hJ6/ZMw2ai0QRwgao1Q==");
-
-  const sendResults = await smsClient.send({
-    from: '+18337292762',
-    to: ["+17652481999"],
-    message: "Hello World 👋🏻 via SMS"
-    //enableDeliveryReport: true
-  });
-
-  // individual messages can encounter errors during sending
-  // use the "successful" property to verify
-  for (const sendResult of sendResults) {
-    if (sendResult.successful) {
-      console.log("Success: ", sendResult);
-    } else {
-      console.error("Something went wrong when trying to send this message: ", sendResult);
+router.post('/sendSms',  function(req, res, next) {
+  new   formidable.IncomingForm().parse(req, async (err, fields, files) => {
+    if (err) {
+      console.error('Error', err)
+      throw err
     }
-  }
+
+    const smsClient = new SmsClient(fields.connectionString);
+    let numbers = fields.toNumbers.split(',')
+    const sendResults = await smsClient.send({
+      from: fields.fromNumber,
+      to: numbers,
+      message: fields.message
+      //enableDeliveryReport: true
+    });
+  
+    // individual messages can encounter errors during sending
+    // use the "successful" property to verify
+    for (const sendResult of sendResults) {
+      if (sendResult.successful) {
+        console.log("Success: ", sendResult);
+      } else {
+        console.error("Something went wrong when trying to send this message: ", sendResult);
+      }
+    }
+  //  console.log('Fields', fields)
+   // console.log('Files', files)
+  //  var fr=new FileReader(); 
+
+ //   console.log(fr.readAsText(files[0])); 
+    // for (const file of Object.entries(files)) {
+    //   console.log(file)
+    // }
+  })
+  console.log(req.files)
+  return false
+ // const smsClient = new SmsClient(req.body.connectionString);
+ console.log(req)
+
  
 });
 router.get('/', function(req, res, next) {
